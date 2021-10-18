0fb372279c18c8feb68c3e14e0c146a2c30f33ff
Fantast1k <83510907+HSFantast1k@users.noreply.github.com>
2021-10-18 13:39:39 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(function () {
+    $('.menu-btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active')
+    })
+
     // Убрать панель управление видео
     // $('[data-fancybox]').fancybox({
     //     youtube: {
@@ -12,6 +16,15 @@ $(function () {
         asNavFor: '.heroes__slider-text',
         prevArrow: '<button class="slick-btn slick-prev" type="button"><img src="images/arrow-left.svg" alt="Prew"></button>',
         nextArrow: '<button class="slick-btn slick-next" type="button"><img src="images/arrow-right.svg" alt="Next"></button>',
+        responsive: [
+            {
+                breakpoint: 789,
+                settings: {
+                    arrows: false,
+                    dots: true,
+                }
+            }
+        ]
     });
     $('.heroes__slider-text').slick({
         slidesToShow: 1,
